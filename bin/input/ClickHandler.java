cc605d0f749331a95004225c8c334c210eac8cc8
Matthew Harrigan <betterthanbossfiredog5@gmail.com>
2022-04-23 07:38:20 -0700

    click framework

---
+++ bin/input/ClickHandler.java
@@ -0,0 +1,31 @@
+package com.input;
+
+import java.util.ArrayList;
+
+public class ClickHandler {
+  // TODO performance gains here using click grid zones (hopefully ill remember what that means)
+
+  private ArrayList<IClickable> clickables;
+
+  public ClickHandler() {
+    this.clickables = new ArrayList<IClickable>();
+  }
+
+  public void register(IClickable clickable) {
+    this.clickables.add(clickable);
+  }
+
+  public void remove(IClickable clickable) {
+
+  }
+
+  public void processClick() {
+
+  }
+
+  public void processMouseMove() {
+
+  }
+
+
+}
---
+++ bin/input/IClickable.java
@@ -0,0 +1,8 @@
+package com.input;
+
+public interface IClickable {
+
+  public boolean isMouseOver(float x, float y);
+  public void cleanFromClickHandler();
+
+}
